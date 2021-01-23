2b1b2392d99bf84e80dfaf6572613a997c2b4e96
oabrivard <olivier@abrivard.fr>
2021-01-22 20:32:58 -0500

    added go tip_calc

---
+++ go/src/chap_1/tip_calc/tip_calc_cli.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+func readPositiveFloat() float64 {
+	for {
+		var line string
+		_,err := fmt.Scanln(&line)
+		if err != nil {panic(err)}
+
+		if value, err := strconv.ParseFloat(line, 64); err == nil && value >= 0 {
+			return value
+		}
+
+		fmt.Print("Please enter a value >= 0 : ")
+	}
+}
+
+func main() {
+	fmt.Print("What is the bill? ")
+	bill := readPositiveFloat()
+
+	fmt.Print("What is the tip percentage? ")
+	tipPercent := readPositiveFloat()
+
+	tip := math.Round(tipPercent * bill) / 100
+
+	fmt.Printf("The tip is %.2f\nThe total is %.2f\n", tip, bill+tip)
+}
